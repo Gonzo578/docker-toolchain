9a0cf560ef7d90c3fa0e658df8d7c43cc11db5e8
Ralf Hochhausen <micro@ralf-hochhausen.de>
2020-02-28 20:49:07 +0100

    Fixed Dockerfiles according to cmake3.15

--- cmake-3.15/build.sh
+++ cmake-3.15/build.sh
@@ -1,1 +1,1 @@
-docker build -t toolchain/ubuntu-18.04-cmake-3.10 .
+docker build -t toolchain/ubuntu-18.04-cmake-3.15 .
--- gcc-arm-embedded-2019-q4/build.sh
+++ gcc-arm-embedded-2019-q4/build.sh
@@ -1,1 +1,1 @@
-docker build -t toolchain/ubuntu-18.04-cmake-3.10-arm-gcc-emb-2019q4 .
+docker build -t toolchain/ubuntu-18.04-cmake-3.15-arm-gcc-emb-2019q4 .
